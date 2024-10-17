ca2bddba61cee57891912a4cabce12afd15840cb
SAJID906 <sajidue906@gmail.com>
2024-10-17 16:03:47 +0500

    three commit

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,5 +1,5 @@
 import { User } from "../Model/userModel.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken'
 export const SignUp = async (req, res) => { 
   try {
